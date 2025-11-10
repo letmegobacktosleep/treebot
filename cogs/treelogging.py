7a6046d7a251275bf9b2ca3dc65ac81278529786
letmegobacktosleep <winstonkwan06@gmail.com>
2025-11-10 11:37:12 +1100

    remove dead next_message datetimes from config

--- cogs/treelogging.py
+++ cogs/treelogging.py
@@ -276,6 +276,9 @@ class TreeLoggingCog(commands.Cog):
                         next_message = next_message + timedelta(days=1)
                         config["next_message"].append(next_message.strftime(DATETIME_STRING_FORMAT))
                         await self.config.set_data(guild_id, "status_message", config)
+                    # remove unused next_message
+                    if len(config["next_message"]) > len(config["valid_hours"]):
+                        config["next_message"] = config["next_message"][:1 + len(config["valid_hours"])]
                     # check the config
                     next_message = config["next_message"][i]
                     next_message = datetime.strptime(next_message, DATETIME_STRING_FORMAT)
